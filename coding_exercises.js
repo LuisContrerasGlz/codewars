27fed40786dc3b9b4349ad3f53fd7b452de5407a
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-02-06 01:18:57 -0600

    array average

--- coding_exercises.js
+++ coding_exercises.js
@@ -7,3 +7,11 @@ You can assume that the number is not negative.*/
 function reverseBits (n) {
     return parseInt(n.toString(2).split("").reverse().join(""), 2);
  }
+
+/* It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.*/
+
+function getAverage(marks){
+    return Math.floor(marks.reduce((a, b) => (a + b)) / marks.length);
+ }
