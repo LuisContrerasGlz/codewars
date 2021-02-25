cda24e2116d3edd2a11ebbea466eb728dbe6ad08
LuisContrerasGlz <luis.fco.contreras@gmail.com>
2021-02-25 14:17:33 -0600

    adding Descending Order problem

--- coding_exercises.js
+++ coding_exercises.js
@@ -27,3 +27,25 @@ function fixTheMeerkat(arr) {
     return arr.reverse();
    }
 
+
+/*Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321*/
+   
+   function descendingOrder(n){
+    let convertString = n.toString();
+    let strToArr = convertString.split("");
+    let sortArr = strToArr.sort();
+    let reverArr = sortArr.reverse();
+    let joinArr = sortArr.join("");
+    let resultNum = Number(joinArr);
+    
+    return resultNum
+    
+  }
